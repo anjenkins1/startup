2a81cc85afabd41b26fd13d4726c92c668b3fc29
Andrew J <jenk2319@byu.edu>
2024-03-15 19:37:23 -0600

    user database code added

--- database.js
+++ database.js
@@ -13,7 +13,30 @@ const reactionCollection = db.collection('reaction');
 (async function testConnection() {
   await client.connect();
   await db.command({ ping: 1 });
+  console.log(`Connected to database`);
 })().catch((ex) => {
   console.log(`Unable to connect to database with ${url} because ${ex.message}`);
   process.exit(1);
 });
+
+function getUser(email) {
+    return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+return userCollection.findOne({ token: token });
+}
+
+async function createUser(email, password) {
+// Hash the password before we insert it into the database
+const passwordHash = await bcrypt.hash(password, 10);
+
+const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+};
+await userCollection.insertOne(user);
+
+return user;
+}
