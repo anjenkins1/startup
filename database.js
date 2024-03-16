42dedf93c254d58b944e7d4f4b68a04e46cff6f7
Andrew J <jenk2319@byu.edu>
2024-03-15 19:29:35 -0600

    setup database file and requirements for index.js

---
+++ database.js
@@ -0,0 +1,19 @@
+const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('startup');
+const userCollection = db.collection('user');
+const reactionCollection = db.collection('reaction');
+
+// This will asynchronously test the connection and exit the process if it fails
+(async function testConnection() {
+  await client.connect();
+  await db.command({ ping: 1 });
+})().catch((ex) => {
+  console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+  process.exit(1);
+});
